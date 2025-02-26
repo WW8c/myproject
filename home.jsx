7e2aa65057db7541ab7b2213925d67a5ec2cfd67
WW8c <WW8@webwrite.co>
2025-02-26 16:45:04 +0500

    change (#24)
    * footer add
    
    * home page add
    
    * new heading add
    
    * text add
    
    * changes add (#20)
    
    * My new branch (#19)
    
    * footer add
    
    * home page add
    
    * new heading add
    
    * Update home.jsx
    
    * new text add
    
    * Staging (#23)
    
    * My new branch (#18)
    
    * footer add
    
    * home page add
    
    * new heading add
    
    * changes add (#20)
    
    * My new branch (#19)
    
    * footer add
    
    * home page add
    
    * new heading add
    
    * Update home.jsx
    
    * new text add
    
    * change

--- home.jsx
+++ home.jsx
@@ -5,7 +5,17 @@ const home = () => {
     <div>
 
       <h1>explore git </h1>
+
+
 <h1>hi whats up</h1>
+<p>helllllllo buddy who are you</p>
+
+      <p>hi who are you</p>
+
+
+
+<h1>hi whats up</h1>
+
     </div>
   )
 }
