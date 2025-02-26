a05cf19349f66e3d5c80b7228849c3d411db13f9
WW8c <WW8@webwrite.co>
2025-02-26 14:52:10 +0500

    changes add (#13)
    * Changes add
    
    * header modified
    
    * header modified (#11)
    
    * header modified (#11) (#12)
    
    * add line
    
    * Footer add
    
    * heading add

---
+++ footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div>
+     <h1>welocome to Github</h1>
+    </div>
+  )
+}
+
+export default footer
---
+++ header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div>
+      <p>Hammad</p>
+      <p>welocome</p>
+
+    </div>
+  )
+}
+
+export default header
