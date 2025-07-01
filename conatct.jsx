f1f0b32e76482ffca12ac61758167964fd42e477
Hammad Ansar <WW8@webwrite.co>
2025-07-01 15:14:24 +0500

    text add

--- conatct.jsx
+++ conatct.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const conatct = () => {
   return (
     <div>
-      <h1>Hello G</h1>
+      <h1>Hello world</h1>
       <p>nice to talk to you</p>
       <p>hbindibidbscd</p>
       <p>my name is hammad</p>
