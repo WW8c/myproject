9b0987232986e7850edc8a96721eb3b03e41b4a2
Hammad Ansar <WW8@webwrite.co>
2025-08-11 14:32:12 +0500

    new file add

---
+++ schedule.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const schedule = () => {
+  return (
+    <div>
+      <h1>Schedule</h1>
+      <p>Welcome to the schedule page!</p>
+    </div>
+  )
+}
+
+export default schedule
