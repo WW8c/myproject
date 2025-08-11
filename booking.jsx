3f71fd1b36bffe6017ab438eb14da9e4a89ecdae
Hammad Ansar <WW8@webwrite.co>
2025-08-11 14:15:23 +0500

    new file added

---
+++ booking.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const booking = () => {
+  return (
+    <div>
+      <h1>Booking</h1>
+      <p>Welcome to the booking page!</p>
+    </div>
+  )
+}
+
+export default booking
